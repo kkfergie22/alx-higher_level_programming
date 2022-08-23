1d22c809de4766fb217e5763373ed9d013f0c1bd
Christian Koranteng <korantengchristian@gmail.com>
2022-08-23 14:53:09 +0000

    Update repo

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,69 +1,43 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * _realloc - Reallocates a memory block
- * @ptr: The pointer to the previous memory block
- * @old_size: The size of the old memory block
- * @new_size: The size of the new memory block
- *
- * Return: The pointer to the new memory block otherwise NULL
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *new_ptr;
-	unsigned int min_size = old_size < new_size ? old_size : new_size;
-	unsigned int i;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr != NULL)
-	{
-		if (new_size == 0)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		new_ptr = malloc(new_size);
-		if (new_ptr != NULL)
-		{
-			for (i = 0; i < min_size; i++)
-				*((char *)new_ptr + i) = *((char *)ptr + i);
-			free(ptr);
-			return (new_ptr);
-		}
-		free(ptr);
-		return (NULL);
-	}
-	else
-	{
-		new_ptr = malloc(new_size);
-		return (new_ptr);
-	}
-}
-
-
-
-/**
- * check_cycle - checks if a singly linked list has a cycle in it
- * @list: list head
- * Return: 0 if no, 1 if yes
- */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *fast, *slow = list;
+	listint_t *p2;
+	listint_t *prev;
 
-	if (list == NULL)
-		return (0);
-
-	fast = list->next;
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
 	{
-		if (slow == fast)
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
 			return (1);
-		fast = fast->next->next;
-		slow = slow->next;
+		}
 	}
+
 	return (0);
 }
