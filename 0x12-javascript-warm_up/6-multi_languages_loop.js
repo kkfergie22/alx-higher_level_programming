c9e40178071b183e81f9d4353f3f5f626850f13c
Christian Koranteng <korantengchristian@gmail.com>
2022-11-28 23:16:19 +0000

    Update task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const words = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+for (const myArray of words) {
+  console.log(myArray);
 }
