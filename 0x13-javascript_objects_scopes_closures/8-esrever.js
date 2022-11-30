915ab21448ab72a08b979b44398487c3ba334014
Christian Koranteng <korantengchristian@gmail.com>
2022-11-30 04:37:00 +0000

    Final update 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,10 @@
 #!/usr/bin/usr/node
 
 exports.esrever = function (list) {
-  const reversedlist = [];
-  for (let i = 0; i < list.length; i++) {
-    reversedlist.push(list.pop()); // push() adds an element to the end of an array
+  const reversedList = [];
+  const listLength = list.length;
+  for (let i = 0; i < listLength; i++) {
+    reversedList.push(list.pop()); // push() adds an element to the end of an array
   }
-  return reversedlist;
+  return reversedList;
 };
