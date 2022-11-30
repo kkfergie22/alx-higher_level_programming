22e718bbe495d6749d19c7148aa8f8346222262f
Christian Koranteng <korantengchristian@gmail.com>
2022-11-30 05:06:24 +0000

    Update 9 new

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 exports.logMe = function (item) {
+  const count = 0;
   if (item === undefined) {
     console.log('0: undefined');
   } else {
-    console.log(`${Object.keys(exports).length - 1}: ${item}`); // Object.keys(exports).length - 1 is the number of keys in exports, minus 1
+    console.log(count + ':' + item);
   }
 };
