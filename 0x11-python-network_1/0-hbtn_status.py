ab72c88e63a35a6aacc7557d3562dee3b749015b
Christian Koranteng <korantengchristian@gmail.com>
2022-12-19 17:00:39 +0000

    Upload all python 1

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for fetching the status of https://alx-intranet.hbtn.io/status
+"""
+
+import urllib.request
+
+
+def get_status():
+    """
+    Fetches the status of https://alx-intranet.hbtn.io/status
+    and returns the body of the response as a bytes object
+    """
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as url:
+        return url.read()
+
+
+if __name__ == "__main__":
+    # Get the status and print the body of the response
+    status = get_status()
+    print("Body response:")
+    print("\t- type:", type(status))
+    print("\t- content:", status)
+    print("\t- utf8 content:", status.decode('utf-8'))
