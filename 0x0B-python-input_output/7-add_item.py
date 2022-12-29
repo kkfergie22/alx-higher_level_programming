af6c4d17e064b60f3aec098d65fed5ca239ab191
Christian Koranteng <korantengchristian@gmail.com>
2022-12-29 07:53:27 +0000

    Correct 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+if __name__ == "__main__":
+    def create_file():
+        '''creates a json file and returns py_list'''
+        py_list = []
+        save_to_json_file(py_list, "add_item.json")
+        py_list = load_from_json_file("add_item.json")
+        return py_list
 
     try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        py_list = load_from_json_file("add_item.json")
+    except (FileNotFoundError):
+        py_list = create_file()
+
+    py_list = load_from_json_file("add_item.json")
+    for i in range(len(argv)):
+        if i != 0:
+            py_list.append(argv[i])
+    save_to_json_file(py_list, "add_item.json")
+    py_list = load_from_json_file("add_item.json")
