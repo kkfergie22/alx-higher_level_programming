5c0d83b43b3b8ef549fb3c2c1026158a01784d13
Christian Koranteng <korantengchristian@gmail.com>
2022-12-29 08:33:46 +0000

    Fix catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a request to 0.0.0.0:5000/catch_me and displays the body of the response.
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "You got me!"
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
