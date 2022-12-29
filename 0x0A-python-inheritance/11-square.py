28180f9a3b7837bec97cbe1a8fb2d98d1769ebcb
Christian Koranteng <korantengchristian@gmail.com>
2022-12-29 07:35:01 +0000

    Correct pycodestyle errors

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,16 +9,17 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """square Class"""
+
     def __init__(self, size):
-            """init"""
-            self.integer_validator("size", size)
-            super().__init__(size, size)
-            self.__size = size
+        """init"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
-            """ area of square """
-            return self.__size ** 2
+        """ area of square """
+        return self.__size ** 2
 
     def __str__(self):
-            """returns when called"""
-            return "[Square] {}/{}".format(self.__size, self.__size)
+        """returns when called"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
