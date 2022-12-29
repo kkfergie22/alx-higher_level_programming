d54b0214aeeb8fdd472a9b52a5fe07278aeec453
Christian Koranteng <korantengchristian@gmail.com>
2022-12-29 09:01:57 +0000

    Fix 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,11 +6,11 @@ display your id
 '''
 
 import requests
+from requests.auth import HTTPBasicAuth
 import sys
 
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
-    user = sys.argv[1]
-    password = sys.argv[2]
-    r = requests.get(url, auth=(user, password))
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get(url, auth=auth)
     print(r.json().get('id'))
